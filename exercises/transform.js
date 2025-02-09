cfd39b3dab2f25c5ca6d5a7361c2b1ea7fd7cd0a
Juan Ruiz <jdruiz008@gmail.com>
2025-02-09 14:17:31 -0500

    add: ejercicio 4/transform.js

---
+++ exercises/transform.js
@@ -0,0 +1,53 @@
+//Datos
+const datos = [
+    {
+        id: 1,
+        nombre: 'Juan',
+        habilidades: ['JavaScript', 'HTML', 'CSS'],
+        proyectos: [
+            { id: 1, nombre: 'Proyecto 1' },
+            { id: 2, nombre: 'Proyecto 2' }
+        ]
+    },
+    {
+        id: 2,
+        nombre: 'María',
+        habilidades: ['Python', 'SQL', 'Django'],
+        proyectos: [
+            { id: 3, nombre: 'Proyecto 3' },
+            { id: 4, nombre: 'Proyecto 4' }
+        ]
+    },
+    {
+        id: 3,
+        nombre: 'Pedro',
+        habilidades: ['Java', 'Spring', 'Hibernate'],
+        proyectos: [
+            { id: 5, nombre: 'Proyecto 5' },
+            { id: 6, nombre: 'Proyecto 6' }
+        ]
+    }
+];
+
+//Función 1
+const desarrolladoresJavascript = (datos,language = 'JavaScript') =>{
+    return datos.filter(dato => {
+        return  dato.habilidades.includes(language)
+    })
+}
+const result1 = desarrolladoresJavascript(datos)
+console.log(JSON.stringify(result1,null,2))
+
+
+//Función 2
+
+const nombresProyectos = (datos) => {
+    let proyectosList = []
+    datos.map(dato => dato.proyectos.map(proyecto => {
+        proyectosList.push(proyecto.nombre)
+    }))
+    return proyectosList
+}
+
+const resul2 = nombresProyectos(datos)
+console.log(resul2)
